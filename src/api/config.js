50beb67a175f7f1a67d5b2c93068ed3a14ee6c3e
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-18 09:30:58 -0300

    config youtube api

---
+++ src/api/config.js
@@ -0,0 +1,1 @@
+export const API_KEY = "AIzaSyCaG3gcz9wHwT8u_fVexCNLpVhmsv3fkVE";
---
+++ src/api/youtube.js
@@ -0,0 +1,12 @@
+import axios from "axios";
+import { API_KEY } from "./config";
+
+export const youtube = axios.create({
+  baseURL: "https://www.googleapis.com/youtube/v3",
+  params: {
+    part: "snippet",
+    type: "video",
+    maxResult: 5,
+    key: API_KEY,
+  },
+});
