cb4addb56c7b044a6be85b4fa88d60813e94332f
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-18 10:01:58 -0300

    implemented request for api

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
+import { youtube } from "../api/youtube";
 import { SearchBar } from "./SearchBar";
 
 export class App extends React.Component {
+  state = { videos: [] };
+
+  async requestTerm(term) {
+    const response = await youtube.get("/search", { params: { q: term } });
+    this.setState({ videos: response.data.items });
+  }
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.requestTerm.bind(this)} />
       </div>
     );
   }
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -3,22 +3,22 @@ import React from "react";
 export class SearchBar extends React.Component {
   state = { term: "" };
 
-  onInputChange(e) {
-    this.setState({ term: e.target.value });
-    console.log(this.state.term);
+  handleSubmit(event) {
+    event.preventDefault();
+    this.props.onFormSubmit(this.state.term);
   }
 
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form onSubmit={this.handleSubmit.bind(this)} className="ui form">
           <div className="field">
             <label htmlFor="search">Video Search</label>
             <input
               type="text"
               id="search"
               value={this.state.term}
-              onChange={this.onInputChange.bind(this)}
+              onChange={(e) => this.setState({ term: e.target.value })}
             />
           </div>
         </form>
