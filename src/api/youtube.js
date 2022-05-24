e1bfe44ddf2df65199ceb3d32294c47e946b0337
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-24 13:10:54 -0300

    migrate syntax class to function

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,11 +1,11 @@
-import axios from "axios";
-import { API_KEY } from "./config";
+import axios from 'axios';
+import { API_KEY } from './config';
 
 export const youtube = axios.create({
-  baseURL: "https://www.googleapis.com/youtube/v3",
+  baseURL: 'https://www.googleapis.com/youtube/v3',
   params: {
-    part: "snippet",
-    type: "video",
+    part: 'snippet',
+    type: 'video',
     maxResult: 10,
     key: API_KEY,
   },
--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,50 +1,43 @@
-import React from 'react';
+import { useState, useRef } from 'react';
 
-export class SearchBar extends React.Component {
-  constructor(props) {
-    super(props);
+export function SearchBar({ onFormSubmit }) {
+  const [term, setTerm] = useState('');
+  const searchRef = useRef();
 
-    this.state = { term: '' };
-
-    this.searchRef = React.createRef();
-  }
-
-  handleSubmit(event) {
+  const handleSubmit = function (event) {
     event.preventDefault();
-    this.props.onFormSubmit(this.state.term);
 
-    this.searchRef.current.blur();
-  }
+    searchRef.current.blur();
+    onFormSubmit(term);
+  };
 
-  render() {
-    return (
-      <nav className="w-full py-3 fixed bg-gray-700 flex justify-center items-center">
-        <form
-          className="border bg-gray-500 border-gray-500 flex items-center w-full max-w-2xl h-12"
-          onSubmit={this.handleSubmit.bind(this)}
+  return (
+    <nav className="w-full py-3 fixed bg-gray-700 flex justify-center items-center">
+      <form
+        className="border bg-gray-500 border-gray-500 flex items-center w-full max-w-2xl h-12"
+        onSubmit={handleSubmit}
+      >
+        <input
+          className="bg-gray-900 border-none h-full outline-1 outline-blue-600 text-white flex-1 px-4 placeholder:text-gray-100"
+          ref={searchRef}
+          value={term}
+          onChange={e => setTerm(e.target.value)}
+          type="search"
+          aria-label="Search a video"
+          placeholder="Search"
+          required
+        />
+        <button
+          className="h-full w-16 flex items-center justify-center border-none bg-transparent text-white"
+          type="submit"
+          aria-label="search"
         >
-          <input
-            className="bg-gray-900 border-none h-full outline-1 outline-blue-600 text-white flex-1 px-4 placeholder:text-gray-100"
-            ref={this.searchRef}
-            value={this.state.term}
-            onChange={e => this.setState({ term: e.target.value })}
-            type="search"
-            aria-label="Search a video"
-            placeholder="Search"
-            required
-          />
-          <button
-            className="h-full w-16 flex items-center justify-center border-none bg-transparent text-white"
-            type="submit"
-            aria-label="search"
-          >
-            <ion-icon
-              name="search-outline"
-              style={{ fontSize: '24px' }}
-            ></ion-icon>
-          </button>
-        </form>
-      </nav>
-    );
-  }
+          <ion-icon
+            name="search-outline"
+            style={{ fontSize: '24px' }}
+          ></ion-icon>
+        </button>
+      </form>
+    </nav>
+  );
 }
