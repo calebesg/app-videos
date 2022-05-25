38f71343306f805801899f8e12a0ef9f97733aff
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-25 15:17:21 -0300

    refactor the navbar

--- src/pages/SearchPage.jsx
+++ src/pages/SearchPage.jsx
@@ -1,3 +1,17 @@
+import { NavBar } from '../components/NavBar';
+import { VideoList } from '../components/VideoList';
+import { useVideos } from '../hooks/useVideos';
+
 export function SearchPage() {
-  return <h1>Search Page</h1>;
+  const [videos] = useVideos('games');
+
+  return (
+    <>
+      <NavBar />
+
+      <main className="pt-16">
+        <VideoList size="small" videos={videos} />
+      </main>
+    </>
+  );
 }
