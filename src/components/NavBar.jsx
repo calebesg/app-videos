a24a5bab4b3bf6f5571f7528e854320673d60399
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-30 14:08:51 -0300

    added logo

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,22 @@
+import { Link } from 'react-router-dom';
 import { SearchForm } from './SearchForm';
+import logo from '../assets/logo.svg';
 
-export function NavBar() {
+export function NavBar({ children }) {
   return (
     <header className="w-full py-2 px-4 fixed z-40 bg-gray-700 backdrop-blur-sm">
-      <nav className="w-full flex justify-center items-center">
+      <nav className="w-full flex justify-center items-center relative">
+        <Link
+          to="/"
+          className="absolute left-0 opacity-0 -z-10 md:z-10 md:opacity-100"
+        >
+          <img src={logo} alt="Youtube" className="w-24" />
+        </Link>
+
         <SearchForm />
       </nav>
+
+      {children}
     </header>
   );
 }
--- src/components/SearchForm.jsx
+++ src/components/SearchForm.jsx
@@ -12,16 +12,16 @@ export function SearchForm() {
 
   return (
     <form
-      className="w-full max-w-xl h-10 bg-gray-500 border border-gray-500 flex items-center"
+      className="w-full max-w-sm md:max-w-md lg:max-w-xl h-10 bg-gray-500 border border-gray-500 flex items-center"
       onSubmit={handleSubmit}
     >
       <input
-        className="bg-gray-900 border-none h-full outline-1 outline-blue-600 text-white flex-1 px-4 placeholder:text-gray-100"
+        className="bg-gray-900 border-none h-full outline-1 outline-blue-600 text-white flex-1 px-2 placeholder:text-gray-100"
         value={term}
         onChange={e => setTerm(e.target.value)}
         type="search"
         aria-label="Search a video"
-        placeholder="Search"
+        placeholder="Pesquisar"
         required
       />
       <button
