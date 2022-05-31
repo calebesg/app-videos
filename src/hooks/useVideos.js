4ec020b113b3b0ba44d4bdfc91c22cbe92551eaf
CalebeSG <calebeguimaraes10@gmail.com>
2022-05-31 13:27:24 -0300

    add new search methods in useVideos

--- src/hooks/useVideos.js
+++ src/hooks/useVideos.js
@@ -2,17 +2,29 @@ import { useState, useEffect } from 'react';
 // import { youtube } from '../api/youtube';
 import { youtube } from '../api/mock-api';
 
-export function useVideos(defaultSearchTerm) {
+export const useVideos = function () {
   const [videos, setVideos] = useState([]);
-
-  useEffect(() => {
-    defaultSearchTerm && searchVideos(defaultSearchTerm);
-  }, [defaultSearchTerm]);
 
   const searchVideos = async function (term) {
     const { data } = await youtube.get('/search', { params: { q: term } });
     setVideos(data.items);
   };
 
-  return [videos, searchVideos];
-}
+  // Busca videos relacionados => Usado na watch page
+  const searchVideosById = async function (id) {
+    const { data } = await youtube.get('/search', {
+      params: { relatedToVideoId: id },
+    });
+    setVideos(data.items);
+  };
+
+  // Videos mais populares => Usado na home page
+  const searchMostPopularVideos = async function () {
+    const { data } = await youtube.get('/videos', {
+      params: { chart: 'mostPopular' },
+    });
+    setVideos(data.items);
+  };
+
+  return [videos, searchVideos, searchVideosById, searchMostPopularVideos];
+};
